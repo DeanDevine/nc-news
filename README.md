0ef39c310bfd6f53f5857ef2a446838247ce9140
DeanDevine <deanrussondevine@gmail.com>
2023-07-18 14:31:25 +0100

    allow user to vote on article

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 I'm not a fan of this double return. I think it would be better to extract out the article card into it's own component and map through each article to return a card for each one
 
 Although we say CSS at this stage is not important, responsiveness is important and changing the size of the browser, your app is not responsive and does not resize - it would be good to add this in!
+
+
+    when i post a comment as a user that doesn't exist a 404: Not Found error pops up, it would be nice if this said why i couldn't post i.e not an existing user (in fact like your other errors do! they look good)
+
+    i love that with a slow connection it says 'posting comment' but i can still click the post button resulting in several of the same comment posting when the request goes through - could be nice to disable the post button while it's posting!
+
+    currently the user has to type in an existing user to post, which is only good if they know them!
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Comments from "./Comments";
-import { getArticle } from "./api";
+import { getArticle, patchArticle } from "./api";
 
 function Article() {
   const [article, setArticle] = useState({});
+  const [articleVotes, setArticleVotes] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
+  const [apiError, setApiError] = useState(null);
   const { article_id } = useParams();
 
   useEffect(() => {
@@ -14,6 +16,30 @@ function Article() {
       setArticle(articleData);
     });
   }, []);
+
+  const handleClickVoteUp = () => {
+    patchArticle(article_id, 1)
+      .then((updatedArticle) => {
+        setArticleVotes((currentArticleVotes) => {
+          return currentArticleVotes + 1;
+        });
+      })
+      .catch((err) => {
+        setApiError(err.response);
+      });
+  };
+
+  const handleClickVoteDown = () => {
+    patchArticle(article_id, -1)
+      .then((updatedArticle) => {
+        setArticleVotes((currentArticleVotes) => {
+          return currentArticleVotes - 1;
+        });
+      })
+      .catch((err) => {
+        setApiError(err.response);
+      });
+  };
 
   return (
     <section>
@@ -25,8 +51,15 @@ function Article() {
         <div className="article-body">{article.body}</div>
         <p>Posted by: {article.author}</p>
         <p>Created at: {new Date(article.created_at).toDateString()}</p>
-        <p>Votes: {article.votes}</p>
+        <p>Votes: {article.votes + articleVotes}</p>
         <p>Comments: {article.comment_count}</p>
+        <button onClick={handleClickVoteUp}>Vote up</button>
+        <button onClick={handleClickVoteDown}>Vote down</button>
+        {apiError ? (
+          <p>
+            {apiError.response.status}: {apiError.response.data.msg}
+          </p>
+        ) : null}
       </div>
       <Comments article_id={article_id} />
     </section>
